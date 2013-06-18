435f73cc010f9896b3c930a3bb7fa51532b8869e
Pavel <marchukpavelp@gmail.com>
2013-06-19 00:45:52 +0300

    db and modules settings configuration

--- ccp/settings.py
+++ ccp/settings.py
@@ -8,6 +8,18 @@ ADMINS = (
     ('Pavel', 'marchukpavelp@gmail.com'),
 )
 
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'ccp.db'),  # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -113,11 +125,10 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     # my apps
-    'bio',
+    # 'bio',
     # installed apps
     'south',
     'django_nose',
-    'registration',
 )
 
 # CRON_CLASSES = [
