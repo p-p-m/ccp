64b82bc3d89e3eee1ea3f9b0f7a2ad37f8b09298
Pavel <marchukpavelp@gmail.com>
2013-06-28 18:35:19 +0300

    ticket 8: test done

--- ccp/bio/templatetags/admin_edit_tags.py
+++ ccp/bio/templatetags/admin_edit_tags.py
@@ -9,4 +9,7 @@ def edit_link(obj):
     >>> edit_link(adm)
     '/admin/auth/user/1/'
     '''
-
+    app = obj._meta.app_label
+    module = obj._meta.module_name
+    return reverse(
+        'admin:{0}_{1}_change'.format(app, module), args=[obj.id])
