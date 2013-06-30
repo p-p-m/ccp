c06c27b1430ca2eb288777edf6b439c060aac3dd
Pavel <marchukpavelp@gmail.com>
2013-06-30 19:33:47 +0300

    ticket 9: manage.py command

---
+++ ccp/bio/management/commands/modelscount.py
@@ -0,0 +1,18 @@
+import sys
+
+from django.core.management.base import BaseCommand
+from django.db import models
+
+
+def _print(s):
+    sys.stdout.write(s+'\n')
+    sys.stderr.write(s+'\n')
+
+
+class Command(BaseCommand):
+    args = 'no args for this command'
+    help = 'Shows list of django models and objects count in them'
+
+    def handle(self, *args, **options):
+        for m in models.get_models():
+            _print(":".join([m.__name__, str(m.objects.count())]))
